0133211074ac8ec3a264e0b7ae83292285ec4aad
Silvia Furegato - silvia.furegato@studio.unibo.it <silvia.furegato@studio.unibo.it>
2024-04-12 12:14:57 +0200

    Updated axsios link and started fixing image pages modification

--- php/api/api-edit.php
+++ php/api/api-edit.php
@@ -2,8 +2,40 @@
 	require_once '../../bootstrap.php';
 
 	$result["editSuccess"] = false;
+	$result["addedPage"] = false;
 
-	if(isset($_POST["testId"]) && isset($_POST["name"]) && isset($_POST["pages"])) {
+	if(isset($_POST["testId"]) && isset($_POST["name"])) {
+		$result["editSuccess"] = $dbh->modifyTest($_POST["testId"], $_POST["name"]);
+	}
+
+	if(isset($_POST["testId"]) && isset($_POST["page"])) { //TODO: Forse potrei farlo direttamente nel databaseHelper
+		$page = json_decode($_POST["page"], true);
+		$dbPages = $dbh->getTestPages($_POST["testId"]);
+
+
+		$oldPage;
+			if (!isset($page["id"])) {
+				createPage();
+			} else {
+				$oldPage = $page;
+
+				$result["db"] = $dbPages;
+				$result["if"] = Array();
+				foreach ($dbPages as $dbPage) {
+					$key = array_search($dbPage["id"], array_column($oldPages, 'id'));
+					array_push($result["if"], $key);
+					if ($key === false) {
+						$dbh->removeTestPage($dbPage["id"]);
+					}
+				}
+			}
+		$result["old"] = $oldPage;
+
+
+		$result["addedPage"] = true;
+	}
+
+	/*if(isset($_POST["testId"]) && isset($_POST["name"]) && isset($_POST["pages"])) {
 		$result["editSuccess"] = $dbh->modifyTest($_POST["testId"], $_POST["name"]);
 		$pages = json_decode($_POST["pages"], true);
 		$dbPages = $dbh->getTestPages($_POST["testId"]);
@@ -12,7 +44,16 @@
 		$oldPages = Array();
 		for ($i = 0; $i < count($pages); $i++) {
 			if (!isset($pages[$i]["id"])) {
-				$dbh->addTestPage($pages[$i]["name"], $_POST["testId"], $pages[$i]["link"], $pages[$i]["image"], $pages[$i]["text"], $pages[$i]["maxTime"]);
+				if (isset($_FILES["imgFile"])) { // Add image page
+					$pageId = $dbh->addTestPage($page["name"], $_POST["testId"], $page["link"], "missing file", $page["text"], $page["maxTime"]);
+					$extension = strtolower(pathinfo($_FILES["imgFile"]["name"], PATHINFO_EXTENSION));
+					$pageName = $_SESSION['id'] . "_" . $_POST["testId"] . "_" . $pageId . "." . $extension;
+					$dbh->updateImageName($pageId, $pageName);
+					$pathCompleta = $imgDir . $pageName;
+					move_uploaded_file($_FILES["imgFile"]["tmp_name"], $pathCompleta);
+				} else { // Add link or text page
+					$pageId = $dbh->addTestPage($page["name"], $_POST["testId"], $page["link"], $page["image"], $page["text"], $page["maxTime"]);
+				}
 			} else {
 				array_push($oldPages, $pages[$i]);
 			}
@@ -29,7 +70,7 @@
 				$dbh->removeTestPage($dbPage["id"]);
 			}
 		}
-	}
+	}*/
 
 	if(isset($_POST["testId"]) && isset($_POST["state"])) {
 		$result["editSuccess"] = $dbh->updateTestState($_POST["testId"], $_POST["state"]);
@@ -37,4 +78,17 @@
 
 	header('Content-Type: application/json');
 	echo json_encode($result);
+
+	function createPage() {//FIXME: Duplicated code
+		if (isset($_FILES["imgFile"])) { // Add image page
+			$pageId = $dbh->addTestPage($page["name"], $_POST["testId"], $page["link"], "missing file", $page["text"], $page["maxTime"]);
+            $extension = strtolower(pathinfo($_FILES["imgFile"]["name"], PATHINFO_EXTENSION));
+            $pageName = $_SESSION['id'] . "_" . $_POST["testId"] . "_" . $pageId . "." . $extension;
+			$dbh->updateImageName($pageId, $pageName);
+            $pathCompleta = $imgDir . $pageName;
+			move_uploaded_file($_FILES["imgFile"]["tmp_name"], $pathCompleta);
+		} else { // Add link or text page
+			$pageId = $dbh->addTestPage($page["name"], $_POST["testId"], $page["link"], $page["image"], $page["text"], $page["maxTime"]);
+		}
+	}
 ?>
--- php/redirector/home-redirector.php
+++ php/redirector/home-redirector.php
@@ -3,7 +3,7 @@
 		
 	$templateParams["title"] = "GazeTracker - Home";
 	$templateParams["pageName"] = "home.php";
-	$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", 
+	$templateParams["js"] = array("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", 
 									"https://cdnjs.cloudflare.com/ajax/libs/heatmap.js/2.0.0/heatmap.min.js", 
 		"../../js/navigation.js", "../../js/testRetriever.js");
 	$templateParams["css"] = array();//"light-background", "register-login", "register");
