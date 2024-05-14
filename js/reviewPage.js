8ecb7f44fdb7a14bab6d11fb4ae66c593e460962
SilviaFuregato <silvia.furegato@studio.unibo.it>
2024-05-14 10:26:48 +0200

    Fixed pages review

--- js/reviewPage.js
+++ js/reviewPage.js
@@ -1,196 +1,14 @@
-/*const buttonContainer = document.getElementById("calibrationPage");
-const pageContainer = document.getElementById("testStage");
-const test = JSON.parse(sessionStorage.getItem("test"));
-
-let calibrationButtons = Array.from(buttonContainer.children);
-calibrationButtons.splice(4,1);
-let calibrationClicks = [5,5,5,5];
-let currentPageIndex = 0;
-let calibrationEnded = false;
-
-
-// Simone
-var xPerc;
-var yPerc;
-var eyeTracker = document.getElementById('testStage');
-var eyeTrackerRect = eyeTracker.getBoundingClientRect();
-var uuid;
-var webgazer;
-
-var calibrazioneFinita= false;
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("CARICAMENTO utente");
-    generateUUID();
-    console.log(uuid);
-    if(test["pages"]){
-        console.log("inizio webgazer");
-        initWebGazer();
-    }else{
-        console.log("pagine non caricate");
-    }
-    
-});
-//---
-
-
-console.log(JSON.parse(sessionStorage.getItem("test")));
-console.log(calibrationButtons);
-console.log(calibrationClicks);
-
-document.getElementById("tmrDuration").parentNode.style.display = "none";
-
-calibrationButtons.forEach(btn => {
-    btn.caller = btn;
-    btn.addEventListener("click", function (event) {
-        event.preventDefault();
-        let btnId = this.classList["value"].substr(-1);
-        if (calibrationClicks[btnId-1] > 1) {
-            calibrationClicks[btnId-1]--;
-            //console.log(btnId + " - " + calibrationClicks[btnId-1]);
-        } else {
-            calibrationClicks[btnId-1] = "✓";
-            if (isCalibrationEnded()) {
-                loadCurrentPage();
-                calibrationEnded = true;
-            }
-        }
-        btn.innerHTML = calibrationClicks[btnId-1];
-    });
-});
-
-setForwardButton();
-
-function isCalibrationEnded() { return calibrationClicks.every(btn => btn == "✓"); }
-
-function loadCurrentPage() {
-    console.log("carico la pagina");
-    console.log(test["pages"]);
-    webgazer.pause();
-    if (test["pages"].length === 0) {
-        pageContainer.innerHTML = `<div>In questo test non ci sono pagine!</div>`;//FIXME: gestire meglio con errore e/o bottone?
-    } else if (test["pages"][currentPageIndex] != null) {
-        drawPage(test["pages"][currentPageIndex]);
-    } else if (currentPageIndex == test["pages"].length) {
-        pageContainer.innerHTML = `<div>QUESTIONARIO!</div>`;//TODO: inserire pagina questionario
-        document.getElementById("tmrDuration").parentNode.style.display = "none";
-    }
-    webgazer.resume();
-}
-
-function drawPage(page) {
-    console.log(document.getElementById("btnForward"));
-    if (page["image"] != null) {
-        pageContainer.innerHTML = `<img src="../../img/` + test["pages"][currentPageIndex]["image"] + `"/>`;
-    } else if (page["link"] != null) {
-        pageContainer.innerHTML = `<iframe scrolling = 'no' onload='onloadIframeEsegui(this)' frameborder = '0' src = "` + test["pages"][currentPageIndex]["link"] + `"></iframe>`;
-    } else {
-        pageContainer.innerHTML = `<div>` + page["text"] + `</div>`;
-    }
-    document.getElementById("tmrDuration").innerHTML = page["max_time"];
-    document.getElementById("tmrDuration").parentNode.style.display = "inline-block";
-}
-
-function setForwardButton() {
-    let btnForward = document.getElementById("btnForward");
-    if (btnForward != null) {
-        btnForward.addEventListener("click", nextPage);
-    }
-}
-
-function nextPage() { 
-    if (currentPageIndex == test["pages"].length) {
-        currentPageIndex = test["pages"].length;
-    } else {
-        currentPageIndex ++;
-    }
-    console.log(currentPageIndex);
-    loadCurrentPage();
-}
-
-// Simone
-function generateUUID() {
-
-    uuid = 'xxxxxxxx-xxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0,
-            v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-
-    const formData = new FormData();
-    formData.append("idPage", test["pages"][currentPageIndex]["id"]);
-    axios.post("../api/api-getAnonymousUser.php", formData
-    ).then(response => {
-        do {
-            uuid = 'xxxxxxxx-xxxx'.replace(/[xy]/g, function (c) {
-                var r = Math.random() * 16 | 0,
-                    v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
-        }
-        while (response.data.some(item => item.IndexUtenteAnonimo === uuid));
-    });
-
-}
-
-// Simone
-function initWebGazer() {
-    webgazer.setGazeListener(function (data, elapsedTime) {
-        if (data == null) {
-            return;
-        }
-        var x = data.x; //these x coordinates are relative to the viewport
-        var y = data.y; //these y coordinates are relative to the viewport
-
-        eyeTrackerRect = eyeTracker.getBoundingClientRect();
-
-        if(calibrationEnded && (currentPageIndex != test["pages"].length)){
-            if (x >= eyeTrackerRect.left && x <= eyeTrackerRect.left + eyeTrackerRect.width &&
-                y >= eyeTrackerRect.top && y <= eyeTrackerRect.top + eyeTrackerRect.height) {
-                coords = trasformaPercentuale(x - eyeTrackerRect.left, y - eyeTrackerRect.top);
-                //console.log(coords.x);
-                console.log("SENDING: " + currentPageIndex);
-                const formData = new FormData();
-                formData.append("coord_x", coords.x);
-                formData.append("coord_y", coords.y);
-                formData.append("idPage", test["pages"][currentPageIndex]["id"]);
-                formData.append("uuid", uuid);
-                axios.post("../api/api-addCoordinate.php", formData
-                ).then(response => {
-                    //console.log(response.data);
-                 });
-            } else {
-                // Il tracciamento è fuori dal quadrato, nascondi il punto di tracciamento degli occhi
-                //console.log('nothing');
-            }
-        }
-    }).begin();
-    webgazer.applyKalmanFilter(webgazer.params.applyKalmanFilter);
-}
-
-// Simone
-function onloadIframeEsegui(e) {
-    e.style.height = e.contentWindow.document.body.scrollHeight + 'px';
-    document.getElementById("preview").style.height = e.style.height;
-}
-
-// Simone
-function trasformaPercentuale(x, y) {
-    xPerc = (x * 100) / eyeTrackerRect.width;
-    yPerc = (y * 100) / eyeTrackerRect.height;
-    //console.log('x:', xPerc, 'y:', yPerc);
-    return { x: xPerc, y: yPerc };
-}*/
-
 const executions = JSON.parse(sessionStorage.getItem("executions"));
 const currentPage = JSON.parse(sessionStorage.getItem("page"));
 const lblPageName = document.getElementById("pageName");
 const lblUserId = document.getElementById("userId");
 const pageContainer = document.getElementById("testStage");
 const heatmapContainer = document.getElementById("heatmap");
+const linemapContainer = document.getElementById("heatmap-canvas");
 const btnHeatmap = document.getElementById("btnHeatmap");
 
 let currentExecIndex = sessionStorage.getItem("currentExecIndex");
+
 setForwardButton();
 setBackwardButton();
 loadPageInformations();
@@ -223,6 +41,7 @@ function loadPageInformations() {
     lblUserId.innerHTML = executions[currentExecIndex];
     drawPage(currentPage);
     //TODO: Update visualization time
+    btnHeatmap.innerHTML = "LINE";
     loadHeatMap();
 }
 
@@ -270,6 +89,10 @@ function onloadIframeEsegui(e) {
 
 // Simone
 function loadHeatMap() {
+    linemapContainer.style.display = "none";
+    heatmapContainer.style.display = "inline-block";
+    heatmapContainer.style.position = "absolute";
+
     heatmapContainer.innerHTML = "";
     heatmapInstance = h337.create({container: heatmapContainer}); // only container is required, the rest will be defaults
     
@@ -299,13 +122,14 @@ function loadHeatMap() {
 
 // Simone
 function loadLineMap() {
-
-    const ctx = document.getElementById("heatmap-canvas").getContext('2d');
-
-    ctx.strokeStyle = 'red'; // Colore della linea (puoi cambiarlo a tuo piacimento)
-    ctx.lineWidth = 1;
-
+    heatmapContainer.style.display = "none";
+    linemapContainer.style.display = "block";
+    const ctx = linemapContainer.getContext('2d');
     ctx.clearRect(0, 0, document.getElementById("heatmap-canvas").width, document.getElementById("heatmap-canvas").height);
+    fix_dpi();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+
     const formData = new FormData();
     formData.append("pageId", currentPage["id"]);
     formData.append("anonymUserId", executions[currentExecIndex]);
@@ -320,6 +144,14 @@ function loadLineMap() {
 
         ctx.stroke();
     });
+}
+
+function fix_dpi() {
+    let dpi = window.devicePixelRatio;
+    let style_height = +getComputedStyle(linemapContainer).getPropertyValue("height").slice(0, -2);
+    let style_width = +getComputedStyle(linemapContainer).getPropertyValue("width").slice(0, -2);
+    linemapContainer.setAttribute('height', style_height * dpi);
+    linemapContainer.setAttribute('width', style_width * dpi);
 }
 
 // Simone - duplicato
--- php/template/reviewPage.php
+++ php/template/reviewPage.php
@@ -1,13 +1,15 @@
 <section class="base-container review-page">
     <section class="firstLine">
-        <div>Pagina: <span id="pageName"></span></div>
-        <div>ID utente: <span id="userId"></span></div>
+        <section>
+            <div>Pagina: <span id="pageName"></span></div>
+            <div>ID utente: <span id="userId"></span></div>
+        </section>
         <div>Tempo impiegato: <span id="tmrDuration">00:01:00</span></div>
-        <button id="btnHeatmap">DOT</button>
+        <button id="btnHeatmap">LINE</button>
     </section>
     <section class="pageContent">
         <section id="testStage"></section>
-        <section style="width: 100%; height: 200px; position: absolute;" id="heatmap"></section>
+        <section id="heatmap"></section>
         <canvas id="heatmap-canvas"></canvas>
     </section>
 </section>
